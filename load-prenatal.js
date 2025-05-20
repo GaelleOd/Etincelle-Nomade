bdcc4fdde69e6d9fc8c7a262b8f1d622be8d1eb9
GaelleOd <odergaelle@gmail.com>
2025-05-20 17:11:04 +0200

    Create load-prenatal.js

---
+++ load-prenatal.js
@@ -0,0 +1,20 @@
+function chargerTexte(id, fichier) {
+    fetch(fichier)
+        .then(response => {
+            if (!response.ok) throw new Error("Erreur de chargement : " + fichier);
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(id).innerText = data;
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById(id).innerText = "[Texte non disponible]";
+        });
+}
+
+const prefix_prenatal = "prenatal";
+["description", "âge", "durée", "nombredeplaces", "créneauxhoraires", "tarif1", "tarif2", "tarif3"]
+.forEach(cle => {
+    chargerTexte(cle + prefix_prenatal, `textes/${cle}${prefix_prenatal}texte.txt`);
+});
